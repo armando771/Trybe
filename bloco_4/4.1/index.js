865ea5b6a95548f24dea25c27862dc18b8f20f45
Armando <a.guerra58@hotmail.com>
2021-04-22 21:19:55 -0300

    ate a 10

--- bloco_4/4.1/index.js
+++ bloco_4/4.1/index.js
@@ -1,17 +1,136 @@
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = {
-  firstName: 'Ana',
-  lastName: 'Santos',
-};
-const patientEmail = 'ana@email.com';
+/*
+let a = 100;
+let b = 66;
+let c = 89;
 
-let base = 5;
-let altura = 8;
+let d = -20;
 
-let area = base * altura;
+ ex:01
+  console.log(a+b)
+  console.log(a-b)
+  console.log(a*b)
+  console.log(a/b)
+  console.log(a%b)
+  ex:02
 
-console.log(area)
+if (a > b) {
+    console.log(a)
+} else {
+    console.log(b)
+} 
+   ex:03
+if (a > b && a > c) {
+    console.log(a)
+} else if (b > a && b > c) {
+    console.log(b)
+} else {
+    console.log(c)
+} 
+ ex:004
 
-let perimetro = base + altura + area;
-console.log(perimetro)
+if (d > 1) {
+    console.log("Positive")
+} else if (d < 0 ) {
+    console.log("Negative")
+} else {
+    console.log("Zero")
+} 
+
+  ex: 005
+let tri1 = 10;
+let tri2 = 50;
+let tri3 = 120;
+let trif = tri1 + tri2 + tri3;
+ ex: 005
+
+if (trif == 180) {
+    console.log("true")
+} else {
+    console.log("false")
+}   
+       ex:006
+
+    let peça = "BisPo"
+
+    switch (peça.toLowerCase()) {
+
+    case  "cavalo":
+    console.log('Anda em L');
+    break;
+
+    case "peao":
+    console.log("Anda 1 diagonal por vez sempre para frente")
+    break;
+
+    case "torre":
+    console.log("Anda em linha reta e para o lado")
+    break;
+ 
+    case "bispo":
+    console.log("Anda na diagonal")   
+    break;
+
+    case "Rainha":
+    console.log("Anda na diagonal para frente e para os lados")   
+    break;
+
+    case "Rei":
+    console.log("Anda uma casa em qualquer direçao de onde esta")   
+    break; 
+                       
+    default:
+        console.log("Peça invalida")
+}  
+   ex:007
+let nota = 80;
+
+if (nota >= 90 && nota < 100 ) {
+    console.log("A")
+} else if (nota >= 80) {
+    console.log("B")       
+} else if (nota >=70) {
+    console.log("C")
+} else if (nota >=60) {
+console.log("D")
+} else if (nota >=50) {
+    console.log("E")
+} else if (nota < 50 && nota > 0) {
+    console.log("F")
+} else { 
+    console.log("Erro Numero invalido")
+}  
+ 
+ EX: 008
+
+let nb1 = 2;
+let nb2 = 2;
+let nb3 = 3;
+
+
+if (nb1 % 2 == 0 | nb2 % 2 == 0 | nb3 % 2 == 0) {
+    console.log(true)
+} else {
+    console.log(false)
+}  
+
+EX: 009
+if (nb1 % 2 != 0 | nb2 % 2 != 0 | nb3 % 2 != 0) {
+    console.log(true)
+} else {
+    console.log(false)
+} 
+
+  Ex: 0010
+
+let preço= 100;
+let venda= 300;
+let lucro;
+
+if(preço < 0 | venda < 0){
+    console.log("erro");
+}
+else{
+    console.log(lucro=(venda-preço* 1.2)*1000);
+}  */
+
+
