fa3ee054cabde7e249fd6c2150b9d83295ea130c
Armando <a.guerra58@hotmail.com>
2021-04-27 17:40:31 -0300

    1

--- bloco_4/4.2/for_array.js
+++ bloco_4/4.2/for_array.js
@@ -1,55 +1,21 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let taskList = ["Estudar", "Fazer exercicio", "Almoçar", "Dormir"]
+// EX:01  console.log(numbers) 
 
+ /*    EX: 002 
+let result = 0;
+for(let index = 0; index < numbers.length; index += 1){
+    result += numbers[index];
+}                                      
+console.log(`A soma do array numbers e : ${result}`);  */
 
-let taskList1 = taskList.indexOf("Almoçar")
+let media = 0;
+let resultado = 0;
 
-console.log(taskList1) 
-
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
-
-for (let frutas = 0; frutas < groceryList.length; frutas +=1 ) {
-     console.log(groceryList[frutas])
-}
+for (let index = 0; index < numbers.length;index =+1) {
+   resultado += numbers[index];
+    media = resultado / numbers[index];
  
-let arrOfNumbers = [10, 20, 30];
-for (let sum of arrOfNumbers) {
-  sum += 5;
-  console.log(sum);  
 } 
-  
 
-// 11
-// 21
-// 31
-
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
-
-for (let contNames of names) {
-    console.log(contNames)
-}
-
-
-let word = 'Hello';
-for (let letter of word) {
-  console.log(letter);
-}
-
-//------------------------------------------------------------------------//
-
-let megaSenaNumbers = [];
-
-let number1 = Math.ceil(Math.random()*60 );
-let number2 = Math.ceil(Math.random()*60 );
-let number3 = Math.ceil(Math.random()*60 );
-let number4 = Math.ceil(Math.random()*60 );
-let number5 = Math.ceil(Math.random()*60 );
-let number6 = Math.ceil(Math.random()*60 );
-
-megaSenaNumbers = [number1,number2,number3,number4,number5,number6];
-console.log(megaSenaNumbers)
-
-// ceil vem de teto - sempre arredonda um numero para cima
-// random gera um numero aleatorio, nesse caso de 0 a 60
-// Math diz que e algo matematico,, numeros
-
+console.log(`A media do array numbers e ${media}`)
