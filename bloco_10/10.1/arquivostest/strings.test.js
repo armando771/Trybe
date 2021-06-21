fb66c5180cdbb068859ea3b691d74eb08f4b51ea
Armando Guerra <82233849+armando771@users.noreply.github.com>
2021-06-21 14:52:18 -0300

    Delete strings.test.js

--- bloco_10/10.1/arquivostest/strings.test.js
+++
@@ -1,9 +0,0 @@
-test('não existe I em team', () => {
-    expect('team').not.toMatch(/I/);
-  });
-  
-  test('mas existe "stop" em Christoph', () => {
-    expect('Christoph').toMatch(/stop/);
-  });
-  
-  // teste em strings 
